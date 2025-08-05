987c5a801f955c9a6ee8bf840df3443eef610cd4
Derek9132 <derek9132@gmail.com>
2025-08-05 17:49:16 -0400

    Added registers, instruction set, memory

--- vm.c
+++ vm.c
@@ -6,7 +6,7 @@
 
 /* VM-related notes */
 // Instruction set - PUSH, POP, ADD, SUB, MUL, DIV, AND, OR, XOR, NOT
-// Store all instructions by converting to bytecode
+// 
 
 
 #include <stdio.h>
@@ -14,12 +14,42 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+typedef enum { // Instruction set
+    INSTRUCTION_PUSH,
+    INSTRUCTION_POP,
+    INSTRUCTION_ADD,
+    INSTRUCTION_SUB,
+    INSTRUCTION_MUL,
+    INSTRUCTION_DIV,
+    INSTRUCTION_AND,
+    INSTRUCTION_OR,
+    INSTRUCTION_XOR,
+    INSTRUCTION_NOT,
+    INSTRUCTION_HALT,
+} Inst_set;
+
+// RAM / Registers
+// Holds a single piece of data to be used by the CPU
+// 16-bit registers
+typedef enum { 
+    REG_A,
+    REG_B,
+    REG_C,
+    REG_D,
+    REG_E,
+    REG_F,
+} Registers;
+
+
+
+#define memory_size (1 << 16) // size of memory (128 KB)
+uint16_t memory[memory_size]; // memory array
+
+
+
 void load() { // load data from disk into memory
     printf("Loading data...\n");
 }
-
-
-
 
 
 int main() { // main loop
